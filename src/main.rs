ed872de692655d06291a15f372fb2a7adc01eab8
AaronLai <Beckwo1015@gmail.com>
2022-03-16 17:04:24 +0800

    testing connection

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,45 @@
-extern crate redis;
-use redis::Commands;
+use actix_web::{
+    get, http, middleware, post, web, App, HttpMessage, HttpResponse, HttpServer, Responder,
+};
+mod middlewares;
+use crate::middlewares::auth::AuthorizationService;
+use reqwest::{Client, StatusCode, Url};
 
-fn fetch_an_integer() -> redis::RedisResult<String> {
-    // connect to redis
-    let client = redis::Client::open("redis://10.124.1.14/")?;
-    let mut con = client.get_connection()?;
-    // throw away the result, just make sure it does not fail
-    let _ : () = con.set("my_key", "42")?;
-    // read back the key and return it.  Because the return value
-    // from the function is a result for integer this will automatically
-    // convert into one.
 
-    con.get("my_key")
+async fn redirect(req: AuthorizationService) -> HttpResponse {
+    let user = req.user;
+    println!("{}",  "https://tride-rust-gixholtv4q-df.a.run.app".to_owned() + &req.path);
 
     
+    let client = Client::new();
+    let mut res = client.get("https://tride-rust-gixholtv4q-df.a.run.app/".to_owned() + &req.path);
+
+    let res = res
+        .header("USER", user)
+        .send()
+        .await
+        .expect("failed to get response")
+        .json::<serde_json::Value>()
+        .await
+        .expect("failed to get payload");
+
+    return   HttpResponse::Ok().json(res);
+    
+
+/*
+    return HttpResponse::Found()
+        .status(http::StatusCode::TEMPORARY_REDIRECT)
+        .append_header(("Location", "https://tride-rust-gixholtv4q-df.a.run.app".to_owned() + &req.path))
+
+        .finish();
+        */
+        
 }
 
-fn main() {
-    let key  =  fetch_an_integer();
-    println!("key: {}", key.unwrap());
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().default_service(web::route().to(redirect)))
+        .bind("0.0.0.0:8000")?
+        .run()
+        .await
 }
